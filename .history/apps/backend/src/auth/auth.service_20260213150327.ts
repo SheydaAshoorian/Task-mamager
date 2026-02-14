da9ee79b15d820c09bc721b6bf07bd48baad164a
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-14 09:44:52 +0330

    add auth entity for user login

---
+++ .history/apps/backend/src/auth/auth.service_20260213150327.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {}
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt'; // اضافه کردن این
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService, 
+  ) {}
+
+  async login(email: string, pass: string) {
+
+    const user = await this.usersService.findOneByEmail(email);
+    
+    if (!user) {
+       throw new UnauthorizedException('کاربر پیدا نشد');
+    }
+
+    const isMatch = await bcrypt.compare(pass, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('رمز عبور اشتباه است');
+    }
+
+    // ساختن توکن (کارت ورود)
+    const payload = { sub: user.id, email: user.email }; // اطلاعاتی که توی توکن مخفی میشه
+    
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user: {
+        name: user.name,
+        family: user.family,
+        email: user.email
+      }
+    };
+  }
+}

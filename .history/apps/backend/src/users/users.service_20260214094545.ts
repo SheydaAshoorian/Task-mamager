e174720d7172a071ba875d9ce87ec6b8d91729f4
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-14 09:46:48 +0330

    edite users.servive

---
+++ .history/apps/backend/src/users/users.service_20260214094545.ts
@@ -0,0 +1,42 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, ...rest } = createUserDto;
+
+    const existingUser = await this.userRepository.findOne({ where: { email } });
+    if (existingUser) {
+      throw new ConflictException('این ایمیل قبلاً ثبت شده است');
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = this.userRepository.create({
+      ...rest,
+      email,
+      password: hashedPassword,
+    });
+
+    return await this.userRepository.save(newUser);
+  }
+
+  async findAll() {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number) {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+}
